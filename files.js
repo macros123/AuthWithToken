f69b31da7ec22f90f82786b63b809ac4c10672d6
macros123 <hit942010@gmail.com>
2020-08-29 00:42:03 +0300

    server v1

--- files.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-async function print(path) {
-    const dir = await fs.promises.opendir(path);
-    for await (const dirent of dir) {
-        console.log(dirent);
-    }
-}
-print('./files').catch(console.error);
